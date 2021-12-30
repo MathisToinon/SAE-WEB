fc1d910db28028b2b38591c44fcb82321b2e3f8b
mathisT <mathis.toinon@etu.univ-lyon1.fr>
2021-12-30 21:55:30 +0100

    index valid w3c (sauf balise dialog)

--- js.js
+++ js.js
@@ -1,7 +1,16 @@
 var $dialog = document.getElementById('mydialog');
-if (!('show' in $dialog)) {
-  document.getElementById('promptCompat').className = 'no_dialog';
-}
-$dialog.addEventListener('close', function () {
-  console.log('Fermeture. ', this.returnValue);
-});
+	if (!('show' in $dialog)) {
+		document.getElementById('promptCompat').className = 'no_dialog';
+	}
+	$dialog.addEventListener('close', function () {
+		console.log('Fermeture. ', this.returnValue);
+	});
+
+
+	var $dialog2 = document.getElementById('mydialog2');
+	if (!('show' in $dialog2)) {
+		document.getElementById('promptCompat2').className = 'no_dialog2';
+	}
+	$dialog2.addEventListener('close', function () {
+		console.log('Fermeture. ', this.returnValue);
+	});
